7ee6acb78bb2f2ba0479985a6b13ed946fe49503
DevGabrielLuiz <142736136+DevGabrielLuiz@users.noreply.github.com>
2024-06-14 18:18:44 -0300

    Update PalavraSecreta.py

--- PalavraSecreta.py
+++ PalavraSecreta.py
@@ -1,11 +1,21 @@
 palavra_secreta = 'arroz'
-palavra_digitada = f'{'*'*len(palavra_secreta)}'
-letra=0
-print(palavra_digitada)
-while len(palavra_secreta)!=len(palavra_digitada) :
-    palavra_digitada=input('Digite uma letra') 
-    if letra in palavra_secreta:
-        palavra_digitada+=palavra_secreta[letra]
+palavra_comparada = f'{'*'*len(palavra_secreta)}'
+i=0
+print(palavra_comparada)
     
-    print(f'{palavra_digitada}')
-    pass
+while palavra_secreta!=palavra_comparada:
+    letra_digitada=input("Digite uma letra:")
+    i+=1
+    if len(letra_digitada)==1 and letra_digitada.isalpha():
+        nova_palavra_comparada = ''
+        for posicao_letra in range(len(palavra_comparada)):
+            if palavra_secreta[posicao_letra] == letra_digitada:
+                nova_palavra_comparada += letra_digitada
+            else:
+                nova_palavra_comparada += palavra_comparada[posicao_letra]        
+        palavra_comparada = nova_palavra_comparada
+    else:
+        print('DIGITE APENAS UMA LETRA')           
+    print(palavra_comparada)
+print(f'O total de tentativas foi: {i}')
+print(f'Finalmente você acertou !! A palavra era: {palavra_secreta}')
