8b281df2338c2b8be2749b8f33eba4b5f91b5fcf
DevGabrielLuiz <142736136+DevGabrielLuiz@users.noreply.github.com>
2024-06-14 12:22:02 -0300

    aula70

---
+++ aula70.py
@@ -0,0 +1,15 @@
+frase = 'O Python é uma linguagem de programação '\
+    'multiparadigma.'\
+    'Python foi criada por Guido van Rossum'
+i = 0
+letra = ''
+letra_count = 0
+while i<len(frase):
+    if frase[i]==' ':
+        i+=1
+        continue
+    if letra_count<=frase.count(frase[i]) :
+        letra=frase[i]
+        letra_count=frase.count(frase[i])
+    i=i+1
+print(f'A letra {letra} aparece {letra_count} vezes!')
