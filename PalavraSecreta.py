88eb9a0eee7314b3ddca0f53471f88a7ad18162d
DevGabrielLuiz <142736136+DevGabrielLuiz@users.noreply.github.com>
2024-06-17 18:42:18 -0300

    Update PalavraSecreta.py

--- PalavraSecreta.py
+++ PalavraSecreta.py
@@ -1,21 +1,19 @@
 palavra_secreta = 'arroz'
-palavra_comparada = f'{'*'*len(palavra_secreta)}'
-i=0
-print(palavra_comparada)
-    
-while palavra_secreta!=palavra_comparada:
-    letra_digitada=input("Digite uma letra:")
-    i+=1
-    if len(letra_digitada)==1 and letra_digitada.isalpha():
-        nova_palavra_comparada = ''
-        for posicao_letra in range(len(palavra_comparada)):
-            if palavra_secreta[posicao_letra] == letra_digitada:
-                nova_palavra_comparada += letra_digitada
-            else:
-                nova_palavra_comparada += palavra_comparada[posicao_letra]        
-        palavra_comparada = nova_palavra_comparada
-    else:
-        print('DIGITE APENAS UMA LETRA')           
-    print(palavra_comparada)
-print(f'O total de tentativas foi: {i}')
-print(f'Finalmente você acertou !! A palavra era: {palavra_secreta}')
+letras_acertadas = ''
+while True:
+    letra_digitada=input("Digite uma letra: ")
+    if len(letra_digitada) >1 and letra_digitada.isalpha() :
+        print('Digite apenas uma letra')
+        continue
+    if letra_digitada in palavra_secreta:
+        letras_acertadas += letra_digitada
+    palavra_formada = ''
+    for letra_secreta in palavra_secreta:
+        if letra_secreta in letras_acertadas:
+            palavra_formada += letra_secreta
+        else:
+            palavra_formada += '*'   
+    print(palavra_formada)
+    if palavra_secreta == palavra_formada:
+        break
+print(palavra_formada)
